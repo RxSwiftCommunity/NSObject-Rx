6d6321848d8af852411df68b84dbe3f34b56e6b7
Ash Furrow <ash@ashfurrow.com>
2015-11-14 17:40:39 -0500

    Lots of locks.

--- NSObject+Rx.swift
+++ NSObject+Rx.swift
@@ -7,18 +7,31 @@ public extension NSObject {
         static var DisposeBag = "rx_diposeBag"
     }
 
+    private func doLocked(closure: () -> Void) {
+        objc_sync_enter(self); defer { objc_sync_exit(self) }
+        closure()
+    }
+
     var rx_disposeBag: DisposeBag {
         get {
-            guard let disposeBag = objc_getAssociatedObject(self, &AssociatedKeys.DisposeBag) as? DisposeBag else {
+            var lookup: DisposeBag?
+            doLocked {
+                lookup = objc_getAssociatedObject(self, &AssociatedKeys.DisposeBag) as? DisposeBag
+            }
+
+            guard let disposeBag = lookup else {
                 let newDisposeBag = DisposeBag()
                 self.rx_disposeBag = newDisposeBag
                 return newDisposeBag
             }
+
             return disposeBag
         }
 
         set {
-            objc_setAssociatedObject(self, &AssociatedKeys.DisposeBag, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            doLocked {
+                objc_setAssociatedObject(self, &AssociatedKeys.DisposeBag, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
         }
     }
 }
