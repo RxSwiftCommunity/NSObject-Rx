ab5c60f7cc3d71998cd938e73559c7dc6541d6be
Thibault Wittemberg <thibaultwittemberg@WarpBookPro.local>
2017-10-15 20:46:46 -0400

    feature: rehabilitates the HasDisposeBag protocol
    The HasDisposeBag protocol is back, but doest not share anymore code
    with AnyObject, so there is no collision between self and base references
    
    The HasDisposeBag protocol can only be implemented by a class, as it
    uses objc_setAssociatedObject. It is safer from a memory management
    point of view.

--- Demo/DemoTests/DemoTests.swift
+++ Demo/DemoTests/DemoTests.swift
@@ -3,31 +3,48 @@ import Nimble
 import RxSwift
 import NSObject_Rx
 
+class DisposeBagTest: HasDisposeBag {
+
+}
+
 class Test: QuickSpec {
     override func spec() {
         it("respects setter") {
             var subject = NSObject()
             let disposeBag = DisposeBag()
             subject.rx.disposeBag = disposeBag
-
+            let subjectProtocol = DisposeBagTest()
+            subjectProtocol.disposeBag = disposeBag
             expect(subject.rx.disposeBag) === disposeBag
+            expect(subjectProtocol.disposeBag) === disposeBag
         }
 
         it("diposes when object is deallocated") {
             var executed = false
+            var executedProtocol = false
+
             let variable = PublishSubject<Int>()
+            let variableProtocol = PublishSubject<Int>()
 
             // Force the bag to deinit (and dispose itself).
             do {
                 let subject = NSObject()
+                let subjectProtocol = DisposeBagTest()
+
                 variable.subscribe(onNext: { _ in
                     executed = true
                 }).addDisposableTo(subject.rx.disposeBag)
+
+                variableProtocol.subscribe(onNext: { _ in
+                    executedProtocol = true
+                }).addDisposableTo(subjectProtocol.disposeBag)
             }
 
             // Force a new value through the subscription to test its been disposed of.
             variable.onNext(1)
+            variableProtocol.onNext(1)
             expect(executed) == false
+            expect(executedProtocol) == false
         }
 
         it("disposes using rx.disposeBag") {
